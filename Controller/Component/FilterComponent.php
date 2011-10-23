45aeecbb0329c845b74502c394cd068f12a57fd4
Lubos Remplik <lubos@lubos.me>
2011-10-23 06:16:00 -0700

    FilterComponent amended - initialize method missig

--- Controller/Component/FilterComponent.php
+++ Controller/Component/FilterComponent.php
@@ -1,8 +1,39 @@
 <?php
-
+/**
+ * Filter Component
+ **/
 class FilterComponent extends Object {
+    
+    public $components = array();
 
-	function like($fields, $keywords) {
+    //called before Controller::beforeFilter()
+    function initialize(&$controller, $settings = array()) {
+        // saving the controller reference for later use
+        $this->controller =& $controller;
+    }
+
+    //called after Controller::beforeFilter()
+    function startup(&$controller) {
+    }
+
+    //called after Controller::beforeRender()
+    function beforeRender(&$controller) {
+    }
+
+    //calledafter Controller::render()
+    function shutdown(&$controller) {
+    }
+
+    //called before Controller::redirect()
+    function beforeRedirect(&$controller, $url, $status=null, $exit=true) {
+    }
+
+    function redirectSomewhere($value) {
+        // utilizing a controller method
+        $this->controller->redirect($value);
+    }
+	
+    function like($fields, $keywords) {
 		$conditions = array();
 		$keywords = explode(' ', $keywords);
         foreach($keywords as $keyword) {
